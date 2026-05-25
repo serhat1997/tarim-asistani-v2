014412c4b0c6155ac0dfef31bcaec20a7b30776a
Ad Soyad <serhat.perk15@gmail.com>
2026-05-25 17:34:33 +0300

    lokal ve render veritabani ayari

--- cari/settings.py
+++ cari/settings.py
@@ -85,7 +85,7 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://tarim_db_3e8m_user:LV3nXo5leKO4mKi7zIXqg9DtwrjRs5s6@dpg-d8a3b3p9rddc739o83d0-a/tarim_db_3e8m',
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
